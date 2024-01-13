578bfc44c510f9c00aef22e99a335f28a6424dc5
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-01-13 15:29:30 +0300

    Plus button added to middle of the tab bar, the ability to hide and unlock passwords is introduced

--- PoDo/UI/Components/DesignableUITextField/DesignableUITextField.swift
+++ PoDo/UI/Components/DesignableUITextField/DesignableUITextField.swift
@@ -1,51 +1,92 @@
-//
-//  DesignableUITextField.swift
-//  PO-DO
-//
-//  Created by Recep Taha Aydın on 27.12.2023.
-//
-
 import UIKit
 
 @IBDesignable
 class DesignableUITextField: UITextField {
-    
+
     // Provides left padding for images
     override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
         var textRect = super.leftViewRect(forBounds: bounds)
         textRect.origin.x += leftPadding
         return textRect
     }
-    
+
+    // Provides right padding for buttons
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.rightViewRect(forBounds: bounds)
+        textRect.origin.x -= rightPadding
+        return textRect
+    }
+
     @IBInspectable var leftImage: UIImage? {
         didSet {
-            updateView()
+            updateLeftView()
         }
     }
     
-    @IBInspectable var leftPadding: CGFloat = 0
-    
+    @IBInspectable var leftPadding: CGFloat = 0 {
+        didSet {
+            updateLeftView()
+        }
+    }
+
+    @IBInspectable var rightButtonImage: UIImage? {
+        didSet {
+            updateRightButton()
+        }
+    }
+
+    @IBInspectable var rightPadding: CGFloat = 0 {
+        didSet {
+            updateRightButton()
+        }
+    }
+
     @IBInspectable var color: UIColor = UIColor.lightGray {
         didSet {
-            updateView()
+            updateLeftView()
         }
     }
-    
-    func updateView() {
+
+    func updateLeftView() {
         if let image = leftImage {
-            leftViewMode = UITextField.ViewMode.always
+            leftViewMode = .always
             let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
             imageView.contentMode = .scaleAspectFit
             imageView.image = image
-            // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
             imageView.tintColor = color
             leftView = imageView
         } else {
-            leftViewMode = UITextField.ViewMode.never
+            leftViewMode = .never
             leftView = nil
         }
-        
+
         // Placeholder text color
-        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: color])
+        attributedPlaceholder = NSAttributedString(string: placeholder ?? "", attributes: [NSAttributedString.Key.foregroundColor: color])
+    }
+
+    func updateRightButton() {
+        if let image = rightButtonImage {
+            let button = UIButton(type: .custom)
+            button.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+            button.contentMode = .scaleAspectFit
+            button.setImage(image, for: .normal)
+            button.tintColor = color
+            button.addTarget(self, action: #selector(rightButtonTapped), for: .touchUpInside)
+            rightViewMode = .always
+            rightView = button
+        } else {
+            rightViewMode = .never
+            rightView = nil
+        }
+    }
+
+    @objc func rightButtonTapped() {
+        if self.isSecureTextEntry == true {
+            rightButtonImage = UIImage(systemName: "eye.fill")
+            self.isSecureTextEntry = false
+        } else if self.isSecureTextEntry == false {
+            self.isSecureTextEntry = true
+            rightButtonImage = UIImage(systemName: "eye.slash.fill")
+        }
     }
 }
