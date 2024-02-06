0bccb218dbd3ee82783be911bbf9add679788b87
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-02-06 16:25:15 +0300

    New task view controller created and title, desc textfields implemented

---
+++ PoDo/UI/Controllers/CreateTask/CreateTaskViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreateTaskViewController.swift
+//  PoDo
+//
+//  Created by Recep Taha Aydın on 6.02.2024.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
