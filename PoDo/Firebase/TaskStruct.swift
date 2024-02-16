1559e559b172d82f959329171de6e88316ab3159
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-02-16 22:35:03 +0300

    Firebase model recreated, floatingButton added to TasksVC and create new task tab removed from the tabbar and started to TasksVC tableView

--- PoDo/Firebase/TaskStruct.swift
+++ PoDo/Firebase/TaskStruct.swift
@@ -9,39 +9,50 @@ import Foundation
 
 struct TaskModel: Codable {
     
+    var id: String
     var title: String
     var description: String
-    var createdDate: String
-    var taskTime: String
+    var date: String
+    var time: String
     var category: String
-    var status: Int
+    var status: Int // 0 = not started, 1 = in progress, 2 = completed
     var sessionCount: Int
     var completedSessionCount: Int
-
+    var sessionDuration: Int
+    var shortBreakDuration: Int
+    var longBreakDuration: Int
+    
     // Custom initializer to convert from Firestore dictionary
     init(dictionary: [String: Any]) {
+        self.id = dictionary["id"] as? String ?? ""
         self.title = dictionary["title"] as? String ?? ""
         self.description = dictionary["description"] as? String ?? ""
-        self.createdDate = dictionary["createdDate"] as? String ?? ""
-        self.taskTime = dictionary["taskTime"] as? String ?? ""
+        self.date = dictionary["createdDate"] as? String ?? ""
+        self.time = dictionary["taskTime"] as? String ?? ""
         self.category = dictionary["category"] as? String ?? ""
         self.status = dictionary["status"] as? Int ?? 0
         self.sessionCount = dictionary["sessionCount"] as? Int ?? 0
         self.completedSessionCount = dictionary["completedSessionCount"] as? Int ?? 0
+        self.sessionDuration = dictionary["sessionDuration"] as? Int ?? 0
+        self.shortBreakDuration = dictionary["shortBreakDuration"] as? Int ?? 0
+        self.longBreakDuration = dictionary["longBreakDuration"] as? Int ?? 0
     }
 
     // Custom computed property to convert to Firestore dictionary
     var dictionaryRepresentation: [String: Any] {
         return [
+            "id": id,
             "title": title,
             "description": description,
-            "createdDate": createdDate,
-            "taskTime": taskTime,
+            "date": date,
+            "time": time,
             "category": category,
             "status": status,
             "sessionCount": sessionCount,
-            "completedSessionCount": completedSessionCount
+            "completedSessionCount": completedSessionCount,
+            "sessionDuration": sessionDuration,
+            "shortBreakDuration": shortBreakDuration,
+            "longBreakDuration": longBreakDuration
         ]
     }
 }
-
---
+++ PoDo/UI/Controllers/Tasks/TasksViewController.swift
@@ -0,0 +1,64 @@
+//
+//  TasksViewController.swift
+//  PoDo
+//
+//  Created by Recep Taha Aydın on 16.02.2024.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let floatingButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+        button.backgroundColor = .PODORed
+        let image = UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 20, weight: .medium))
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 30
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(floatingButton)
+        floatingButton.addTarget(self, action: #selector(didTapFloatingButton), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let tabBarHeight = tabBarController?.tabBar.frame.size.height ?? 0
+        let floatingButtonY = view.frame.size.height - tabBarHeight - 60 - 20
+        
+        floatingButton.frame = CGRect(x: view.frame.size.width - 70,
+                                      y: floatingButtonY,
+                                      width: 60,
+                                      height: 60)
+    }
+    
+    @objc private func didTapFloatingButton() {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "createTaskVC")
+        self.present(vc, animated: true)
+    }
+}
+
+extension TasksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let topCell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        return topCell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+}
+
+extension TasksViewController: UITableViewDelegate {
+
+}
+
