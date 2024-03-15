f46575df4a9a748ce874f92b765227a2d875db56
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-03-15 16:34:19 +0300

    TabBarController class created for filling TabBarItem images when selected

---
+++ PoDo/UI/Components/Tabbar/TabBarController.swift
@@ -0,0 +1,26 @@
+//
+//  Tabbar.swift
+//  PoDo
+//
+//  Created by Recep Taha Aydın on 15.03.2024.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        let tabBar = self.tabBar
+        
+        let timerSelectImage = UIImage(systemName: "stopwatch.fill")
+        let tasksSelectImage = UIImage(systemName: "list.bullet.rectangle.fill")
+        let analyticsSelectImage = UIImage(systemName: "chart.bar.fill")
+        let settingsSelectImage = UIImage(systemName: "gearshape.fill")
+
+        (tabBar.items![0]).selectedImage = timerSelectImage
+        (tabBar.items![1]).selectedImage = tasksSelectImage
+        (tabBar.items![2]).selectedImage = analyticsSelectImage
+        (tabBar.items![3]).selectedImage = settingsSelectImage
+    }
+}
